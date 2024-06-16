71ffcea0e5db4fd3da499c59bd3bbcc260d21d77
Андрей <amore1302@mail.ru>
2024-06-16 15:54:30 +0300

    2

--- main.py
+++ main.py
@@ -9,6 +9,20 @@ class Monster():
         self.name = name
         self.age = age
 
+class Weapon(ABC):
+    @abstractmethod
+    def attack():
+        pass
+
+class Sword(Weapon):
+    def attack():
+        print("Атакуем мечом")
+
+class Bow(Weapon):
+    def attack():
+        print("Атакуем луком")
+
+
 
 voin = Fighter("Илья Муромец",33)
 raz = Monster("Соловей Разбойник", 300)
